5d66783076c51456072685890cb24df6dc3ed1bb
Allan Jardine <allan@datatables.net>
2022-11-04 14:51:29 +0000

    Missing files in DataTables-DataTables

--- js/dataTables.colReorder.js
+++ js/dataTables.colReorder.js
@@ -1,6 +1,47 @@
+
 /*! ColReorder 1.5.6
  * ©2010-2022 SpryMedia Ltd - datatables.net/license
  */
+
+(function( factory ){
+	if ( typeof define === 'function' && define.amd ) {
+		// AMD
+		define( ['jquery', 'datatables.net'], function ( $ ) {
+			return factory( $, window, document );
+		} );
+	}
+	else if ( typeof exports === 'object' ) {
+		// CommonJS
+		module.exports = function (root, $) {
+			if ( ! root ) {
+				// CommonJS environments without a window global must pass a
+				// root. This will give an error otherwise
+				root = window;
+			}
+
+			if ( ! $ ) {
+				$ = typeof window !== 'undefined' ? // jQuery's factory checks for a global window
+					require('jquery') :
+					require('jquery')( root );
+			}
+
+			if ( ! $.fn.dataTable ) {
+				require('datatables.net')(root, $);
+			}
+
+
+			return factory( $, root, root.document );
+		};
+	}
+	else {
+		// Browser
+		factory( jQuery, window, document );
+	}
+}(function( $, window, document, undefined ) {
+'use strict';
+var DataTable = $.fn.dataTable;
+
+
 
 /**
  * @summary     ColReorder
@@ -1465,3 +1506,7 @@ $.fn.dataTable.Api.register( 'colReorder.disable()', function() {
 		}
 	} );
 } );
+
+
+return DataTable;
+}));
