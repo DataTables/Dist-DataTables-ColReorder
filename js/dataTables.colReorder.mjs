5059172b72da43d9a4cf6ec191643aa08f835c95
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-01-05 09:45:38 +0000

    d3ad89719ae7d3ca64c71657e81277cfa53889d9
    Sync to source repo @d3ad89719ae7d3ca64c71657e81277cfa53889d9

--- js/dataTables.colReorder.mjs
+++ js/dataTables.colReorder.mjs
@@ -489,7 +489,7 @@ $.extend( ColReorder.prototype, {
 	fnEnable: function ( flag )
 	{
 		if ( flag === false ) {
-			return fnDisable();
+			return this.fnDisable();
 		}
 
 		this.s.enable = true;
