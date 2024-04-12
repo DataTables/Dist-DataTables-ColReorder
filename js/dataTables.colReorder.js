8ec6d1226048b1b34c90fcf853c790e16bc3c131
dtbuild <dtbuild@rigil>
2024-04-12 15:40:16 +0000

    13d70d5737091072327efc90f99fdb01cfc3a00b Dev: Attempt to optimise the width get a little but getting them all at once
    153cdcb2afb28d91748370150d559d2bf8568ce6 Types: new constructor returns instance, not void
    
    bd20f90d1c0eaaf20c4fb1fd5e793e9fc47bd084 Release 2.0.0
    
    0cd16741af2a707f6e632b15d816e62270cd5950 Release 2.0.1
    
    Sync to source repo @0cd16741af2a707f6e632b15d816e62270cd5950

--- js/dataTables.colReorder.js
+++ js/dataTables.colReorder.js
@@ -1,53 +1,6 @@
-/*! ColReorder 2.0.0-dev
+/*! ColReorder 2.0.1
  * © SpryMedia Ltd - datatables.net/license
  */
-
-(function( factory ){
-	if ( typeof define === 'function' && define.amd ) {
-		// AMD
-		define( ['jquery', 'datatables.net'], function ( $ ) {
-			return factory( $, window, document );
-		} );
-	}
-	else if ( typeof exports === 'object' ) {
-		// CommonJS
-		var jq = require('jquery');
-		var cjsRequires = function (root, $) {
-			if ( ! $.fn.dataTable ) {
-				require('datatables.net')(root, $);
-			}
-		};
-
-		if (typeof window === 'undefined') {
-			module.exports = function (root, $) {
-				if ( ! root ) {
-					// CommonJS environments without a window global must pass a
-					// root. This will give an error otherwise
-					root = window;
-				}
-
-				if ( ! $ ) {
-					$ = jq( root );
-				}
-
-				cjsRequires( root, $ );
-				return factory( $, root, root.document );
-			};
-		}
-		else {
-			cjsRequires( window, jq );
-			module.exports = factory( jq, window, window.document );
-		}
-	}
-	else {
-		// Browser
-		factory( jQuery, window, document );
-	}
-}(function( $, window, document ) {
-'use strict';
-var DataTable = $.fn.dataTable;
-
-
 
 (function( factory ){
 	if ( typeof define === 'function' && define.amd ) {
@@ -876,17 +829,17 @@ var ColReorder = /** @class */ (function () {
         enable: true,
         order: null
     };
-    ColReorder.version = '2.0.0-dev';
+    ColReorder.version = '2.0.1';
     return ColReorder;
 }());
 
-/*! ColReorder 2.0.0-dev
+/*! ColReorder 2.0.1
  * © SpryMedia Ltd - datatables.net/license
  */
 /**
  * @summary     ColReorder
  * @description Provide the ability to reorder columns in a DataTable
- * @version     2.0.0-dev
+ * @version     2.0.1
  * @author      SpryMedia Ltd
  * @contact     datatables.net
  * @copyright   SpryMedia Ltd.
@@ -1016,10 +969,6 @@ $(document).on('preInit.dt', function (e, settings) {
         }
     }
 });
-
-
-return DataTable;
-}));
 
 
 return DataTable;
--- js/dataTables.colReorder.mjs
+++ js/dataTables.colReorder.mjs
@@ -1,4 +1,4 @@
-/*! ColReorder 2.0.0-dev
+/*! ColReorder 2.0.1
  * © SpryMedia Ltd - datatables.net/license
  */
 
@@ -7,53 +7,6 @@ import DataTable from 'datatables.net';
 
 // Allow reassignment of the $ variable
 let $ = jQuery;
-
-
-(function( factory ){
-	if ( typeof define === 'function' && define.amd ) {
-		// AMD
-		define( ['jquery', 'datatables.net'], function ( $ ) {
-			return factory( $, window, document );
-		} );
-	}
-	else if ( typeof exports === 'object' ) {
-		// CommonJS
-		var jq = require('jquery');
-		var cjsRequires = function (root, $) {
-			if ( ! $.fn.dataTable ) {
-				require('datatables.net')(root, $);
-			}
-		};
-
-		if (typeof window === 'undefined') {
-			module.exports = function (root, $) {
-				if ( ! root ) {
-					// CommonJS environments without a window global must pass a
-					// root. This will give an error otherwise
-					root = window;
-				}
-
-				if ( ! $ ) {
-					$ = jq( root );
-				}
-
-				cjsRequires( root, $ );
-				return factory( $, root, root.document );
-			};
-		}
-		else {
-			cjsRequires( window, jq );
-			module.exports = factory( jq, window, window.document );
-		}
-	}
-	else {
-		// Browser
-		factory( jQuery, window, document );
-	}
-}(function( $, window, document ) {
-'use strict';
-var DataTable = $.fn.dataTable;
-
 
 /**
  * Mutate an array, moving a set of elements into a new index position
@@ -836,17 +789,17 @@ var ColReorder = /** @class */ (function () {
         enable: true,
         order: null
     };
-    ColReorder.version = '2.0.0-dev';
+    ColReorder.version = '2.0.1';
     return ColReorder;
 }());
 
-/*! ColReorder 2.0.0-dev
+/*! ColReorder 2.0.1
  * © SpryMedia Ltd - datatables.net/license
  */
 /**
  * @summary     ColReorder
  * @description Provide the ability to reorder columns in a DataTable
- * @version     2.0.0-dev
+ * @version     2.0.1
  * @author      SpryMedia Ltd
  * @contact     datatables.net
  * @copyright   SpryMedia Ltd.
@@ -976,10 +929,6 @@ $(document).on('preInit.dt', function (e, settings) {
         }
     }
 });
-
-
-return DataTable;
-}));
 
 
 export default DataTable;
