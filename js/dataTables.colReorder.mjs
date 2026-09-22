3d806ac9e4c599d0f62c9813213ef7837768df63
dtbuild <dtbuild@rigil>
2026-09-22 09:30:12 +0000

    2b49cc39c0e130a1ab40e80ae6ea31189b3bb4a7 Release 3.1.0
    Sync to source repo @2b49cc39c0e130a1ab40e80ae6ea31189b3bb4a7

--- js/dataTables.colReorder.mjs
+++ js/dataTables.colReorder.mjs
@@ -1,4 +1,4 @@
-/*! ColReorder 3.0.2 for DataTables
+/*! ColReorder 3.1.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
@@ -886,7 +886,7 @@ ColReorder.defaults = {
     headerRows: null,
     order: null
 };
-ColReorder.version = '3.0.2';
+ColReorder.version = '3.1.0';
 
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
